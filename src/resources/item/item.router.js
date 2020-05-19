06dcb0ff35ce6a6dd39d42b675e908c755d557f4
Aditya Sharma <sharmaaditya570191@gmail.com>
2020-05-19 20:42:50 +0530

    lesson 2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,19 @@ import { Router } from 'express'
 
 const router = Router()
 
+const controller = (req, res) => {
+  res.send({ message: 'hello' })
+}
+
+router
+  .route('/')
+  .get(controller)
+  .post(controller)
+
+router
+  .route('/:id')
+  .get(controller)
+  .put(controller)
+  .delete(controller)
+
 export default router
